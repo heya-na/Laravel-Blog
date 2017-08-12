9347f57aff72b18984eadbc0dd1b91f33ca179f1
Heya Na <naheya1126@gmail.com>
2017-08-12 12:41:59 -0400

    Add a comment

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,11 @@ class Post extends Model
     {
     	return $this->hasMany(Comment::class);
     }
+
+    public function addComment($body){
+    	Comment::create([
+    		'body' => $body,
+    		'post_id' => $this->id
+    	]);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
 	{{ $post->body }}
 	<hr>
 	<div class="comments">
-		<ul>
+		<ul class="list-group">
 			@foreach($post->comments as $comment)
 				<li class="list-group-item">
 				<strong>
@@ -17,6 +17,24 @@
 				</li>
 			@endforeach
 		</ul>
+	</div>
+	
+	<hr>
+	<div class="card">
+		<div class="card-block">
+			<form method="POST" action="/posts/{{ $post->id }}/comments">
+				{{ csrf_field() }}
+				<div class="form-group">
+					<textarea name="body" placeholder="Your comment here." class="form-control"></textarea>
+				</div>
+
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary">Add Comment</button>
+				</div>
+			</form>
+
+			@include('layouts.errors')
+		</div>
 	</div>
 </div>
 @endsection
