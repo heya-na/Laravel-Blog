10da9973efb69f867df2b9b021b932f55dc59842
Heya Na <naheya1126@gmail.com>
2017-08-12 11:35:15 -0400

    show comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,18 @@
 	<h1>{{ $post->title }}</h1>
 
 	{{ $post->body }}
+	<hr>
+	<div class="comments">
+		<ul>
+			@foreach($post->comments as $comment)
+				<li class="list-group-item">
+				<strong>
+					{{ $comment->created_at->diffForHumans() }}: &nbsp;
+				</strong>
+					{{ $comment->body }}
+				</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
 @endsection
