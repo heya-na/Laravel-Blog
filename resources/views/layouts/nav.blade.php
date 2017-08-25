1e463e661a13348684fa2759fa028f556038ac72
Heya Na <naheya1126@gmail.com>
2017-08-24 23:32:59 -0400

    navbar link fix and redirect guest to sign in

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,14 +1,16 @@
 <div class="blog-masthead">
   <div class="container">
     <nav class="blog-nav">
-      <a class="blog-nav-item active" href="#">Home</a>
-      <a class="blog-nav-item" href="#">New features</a>
-      <a class="blog-nav-item" href="#">Press</a>
-      <a class="blog-nav-item" href="#">New hires</a>
+      <a class="blog-nav-item active" href="/">Home</a>
+      <a class="blog-nav-item" href="/posts/create">Publish</a>
+      <a class="blog-nav-item" href="/register">Register</a>
       
       @if(Auth::check())
+        <a class="blog-nav-item navbar-right" href="/logout">Log Out</a>
       	<a class="blog-nav-item navbar-right" href="#">{{ Auth::user()->name }}</a>
-	  @endif
+	    @else
+        <a class="blog-nav-item navbar-right" href="/login">Log In</a>
+      @endif
     </nav>
   </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/posts/create', 'PostController@create');
 Route::post('/posts','PostController@store');
 
 Route::get('/posts/{post}', 'PostController@show');
+
 Route::get('/posts/tags/{tag}', 'TagController@index');
 
 Route::post('/posts/{post}/comments', 'CommentController@store');
@@ -30,7 +31,7 @@ Route::get('/register', 'RegistrationController@create');
 
 Route::post('/register', 'RegistrationController@store');
 
-Route::get('/login', 'SessionController@create');
+Route::get('/login', 'SessionController@create')->name('login');
 Route::post('/login', 'SessionController@store');
 
 Route::get('/logout', 'SessionController@destory');
